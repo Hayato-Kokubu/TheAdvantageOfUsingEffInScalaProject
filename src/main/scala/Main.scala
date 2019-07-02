e394881ccc9b2fd949c30f9173356b120f30f87a
hayatokokubu <kokubu.hayato@gmail.com>
2019-07-02 09:49:41 +0900

    Future 復習 Try型

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,12 +6,28 @@ object Main extends App{
   val fut = Future{
     Thread.sleep(3000); 21 + 21
   }
-  println(fut.isCompleted)
-  println(fut) // <not completed>
-  println(fut.value)
+//  println(fut.isCompleted)
+//  println(fut) // <not completed>
+//  println(fut.value)
+//  Thread.sleep(5000)
+//  println("--- fut is finished ---")
+//  println(fut.isCompleted)
+//  println(fut.value)
+//  println(fut) // Future(Success(42))
+
+
+  // Try型
+  val futFailure = Future{Thread.sleep(3000); 21/0}
+  println(futFailure.isCompleted)
+  println(futFailure) // <not completed>
+  println(futFailure.value)
   Thread.sleep(5000)
   println("--- fut is finished ---")
-  println(fut.isCompleted)
-  println(fut.value)
-  println(fut) // Future(Success(42))
+  println(futFailure.isCompleted)
+  println(futFailure.value)
+  println(futFailure) // Future(Success(42))
+
+
+
+
 }
