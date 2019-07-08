3ff4bb1b690411aa39586ecd95a0f3acf9ae0205
hayatokokubu <kokubu.hayato@gmail.com>
2019-07-08 18:42:18 +0900

    Future[Either[E,T]] の例 (佐藤 さんの例)

---
+++ src/main/scala/ForFutureTry.scala
@@ -0,0 +1,18 @@
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
+import scala.util.{Failure, Success}
+
+object ForFutureTry {
+
+  //  val res = Future{
+  //    Thread.sleep(100) ;
+  //    if (Math.random * 100 < 50) Success("user found") else Failure(UserNotFound)
+  //  }
+
+
+
+
+
+
+}
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,10 +4,32 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.{Failure, Success}
 
 object Main extends App {
+  // Future[Either[E,T]]
 
-  val res = Future{
-    Thread.sleep(100) ;
-    if (Math.random * 100 < 50) Success("user found") else Failure(UserNotFound)
+  def process(input: Input) : Future[Either[Error, Unit]] = {
+    for{
+      r1 <- subProcess1(input)
+
+      r2 <- r1 match {
+        case Right(_) => subProcess2(input)
+        case Left(err1) => Future.successful(Left(err1))
+      }
+
+      _ <- r2 match {
+        case Right(_) => subProcess3(input)
+        case Left(err2) => Future.successful(Left(err2))
+      }
+
+    } yield Right()
   }
+
+  def subProcess1(input: Input): Future[Either[Error, Unit]] = ???
+  def subProcess2(input: Input): Future[Either[Error, Unit]] = ???
+  def subProcess3(input: Input): Future[Either[Error, Unit]] = ???
+
+}
+
+
+case class Input(){
 
 }
