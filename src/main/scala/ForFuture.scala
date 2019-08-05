c7697536ba91dcc45a5e1abe40431eae2fc9345d
Hayato-Kokubu <kokubu.hayato@gmail.com>
2019-08-06 06:24:17 +0900

    ContraVariant

--- src/main/scala/ForFuture.scala
+++ src/main/scala/ForFuture.scala
@@ -1,58 +1,58 @@
-import scala.concurrent.Future
-import scala.util.{Failure, Success}
-
-object ForFuture {
-
-  //  val fut = Future{
-  //    Thread.sleep(3000); 21 + 21
-  //  }
-
-  //  println(fut.isCompleted)
-  //  println(fut) // <not completed>
-  //  println(fut.value)
-  //  Thread.sleep(5000)
-  //  println("--- fut is finished ---")
-  //  println(fut.isCompleted)
-  //  println(fut.value)
-  //  println(fut) // Future(Success(42))
-
-
-  // Try型
-  //  val futFailure = Future{Thread.sleep(3000); 21/0}
-  //  println(futFailure.isCompleted)
-  //  println(futFailure) // <not completed>
-  //  println(futFailure.value)
-  //  Thread.sleep(5000)
-  //  println("--- fut is finished ---")
-  //  println(futFailure.isCompleted)
-  //  println(futFailure.value)
-  //  println(futFailure) // Future(Success(42))
-
-  val res = Future{
-    Thread. sleep(300); if( Math.random * 100 < 50) "User fond!" else UserNotFound
-  }
-
-  println(res)
-  println("---------------")
-  Thread.sleep(1000)
-  println("operation done!")
-  println("---------------")
-  println(res)
-  println(res.value)
-
-  val resOn = res.onComplete{
-    case Success(msg) => s"res succeeded! => $msg"
-    case Failure( e ) => s"res failed!    => ${e.printStackTrace}"
-  }
-
-  //  Scala2.13 ではdeprecate
-  //  res onSuccess {
-  //    case msg => println(s"res succeeded! => $msg")
-  //    case _ => println("wwwwww")
-  //  }
-  //  res onFailure {
-  //    case e => println(s"res failed! => ${e.printStackTrace}")
-  //    case _ => println("wwwwww")
-  //  }
-
-}
+//import scala.concurrent.Future
+//import scala.util.{Failure, Success}
+//
+//object ForFuture {
+//
+//  //  val fut = Future{
+//  //    Thread.sleep(3000); 21 + 21
+//  //  }
+//
+//  //  println(fut.isCompleted)
+//  //  println(fut) // <not completed>
+//  //  println(fut.value)
+//  //  Thread.sleep(5000)
+//  //  println("--- fut is finished ---")
+//  //  println(fut.isCompleted)
+//  //  println(fut.value)
+//  //  println(fut) // Future(Success(42))
+//
+//
+//  // Try型
+//  //  val futFailure = Future{Thread.sleep(3000); 21/0}
+//  //  println(futFailure.isCompleted)
+//  //  println(futFailure) // <not completed>
+//  //  println(futFailure.value)
+//  //  Thread.sleep(5000)
+//  //  println("--- fut is finished ---")
+//  //  println(futFailure.isCompleted)
+//  //  println(futFailure.value)
+//  //  println(futFailure) // Future(Success(42))
+//
+//  val res = Future{
+//    Thread. sleep(300); if( Math.random * 100 < 50) "User fond!" else UserNotFound
+//  }
+//
+//  println(res)
+//  println("---------------")
+//  Thread.sleep(1000)
+//  println("operation done!")
+//  println("---------------")
+//  println(res)
+//  println(res.value)
+//
+//  val resOn = res.onComplete{
+//    case Success(msg) => s"res succeeded! => $msg"
+//    case Failure( e ) => s"res failed!    => ${e.printStackTrace}"
+//  }
+//
+//  //  Scala2.13 ではdeprecate
+//  //  res onSuccess {
+//  //    case msg => println(s"res succeeded! => $msg")
+//  //    case _ => println("wwwwww")
+//  //  }
+//  //  res onFailure {
+//  //    case e => println(s"res failed! => ${e.printStackTrace}")
+//  //    case _ => println("wwwwww")
+//  //  }
+//
+//}
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,13 +1,53 @@
-import scala.concurrent.Future
-import scala.concurrent.ExecutionContext.Implicits.global
-
-import scala.util.{Failure, Success}
+import Implicits._
 
 object Main extends App {
 
-  val res = Future{
-    Thread.sleep(100) ;
-    if (Math.random * 100 < 50) Success("user found") else Failure(UserNotFound)
-  }
+  val ps = implicitly[Printable[String]]
+  val pb = implicitly[Printable[Boolean]]
+
+  println( ps.format("hello") )
+  println( pb.format(true) )
 
 }
+
+
+// self => という書き方は何？
+trait Printable[A] { self =>
+  def format(value: A): String
+  def contramap[B](func: B => A): Printable[B] =
+    new Printable[B] {
+      def format(value: B): String ={
+        self.format(func(value))
+      }
+    }
+}
+
+object Printable{
+  def format[A](value: A)(implicit p: Printable[A]): String =
+    p.format(value)
+
+}
+
+object Implicits {
+  implicit val stringPrintable: Printable[String] =
+    new Printable[String] {
+      def format(value: String): String =
+        "\"" + value + "\""
+    }
+
+  implicit val booleanPrintable: Printable[Boolean] =
+    new Printable[Boolean] {
+      def format(value: Boolean): String =
+        if(value) "yes" else "no"
+    }
+
+  implicit def boxPrintable[A](implicit pa: Printable[A]): Printable[Box[A]] = {
+    new Printable[Box[A]] {
+      def format(box: Box[A]): String = {
+        pa.format(box.value)
+      }
+    }
+  }
+}
+
+final case class Box[A](value: A)
