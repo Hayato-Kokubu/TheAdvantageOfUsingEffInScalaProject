a141c34f3561bf7ec2c09916a50138e53252dc55
hayatokokubu <kokubu.hayato@gmail.com>
2019-08-19 20:44:05 +0900

    eff example Option

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,36 @@
-import cats._, data._
+//import cats._
+//import data._
+import org.atnos.eff.{Eff, Fx}
 
-import org.atnos.eff.eval._        // use delay
-import org.atnos.eff.syntax.eval._ // use runEval
-import org.atnos.eff.syntax.eff._  // use run
+import org.atnos.eff.eval._          // use delay
+import org.atnos.eff.syntax.eval._   // use runEval
+
+import org.atnos.eff.option._        // use fromOption
+import org.atnos.eff.syntax.option._ // use runOption
+
+import org.atnos.eff.syntax.eff._    // use run
+
+
+
+
+
 object Main extends App {
+  type S = Fx.fx1[Option]
 
-  val a = delay(1 + 1).runEval.run
-  println(a)
+  val evalEff = delay(1 + 1).runEval.run
+  println(evalEff)
 
+
+  val map: Map[String, Int] =
+    Map("key1" -> 10, "key2" -> 20)
+
+
+  def addKeys(key1: String, key2: String): Eff[S, Int] = for {
+    a <- fromOption(map.get(key1))
+    b <- fromOption(map.get(key2))
+  } yield a + b
+
+  val optionEff = (addKeys("key1", "key2").runOption.run, addKeys("key1", "missing").runOption.run)
+  println(optionEff)
 
 }
