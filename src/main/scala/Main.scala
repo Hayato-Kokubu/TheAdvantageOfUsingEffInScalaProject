2957fb0da24c7c7f0bc589e3fcf0c734c650578a
hayatokokubu <kokubu.hayato@gmail.com>
2019-08-19 21:35:30 +0900

    either ==== 未解決。。。

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,52 +1,45 @@
 import org.atnos.eff.{Eff, Fx}
 
-//import org.atnos.eff.eval._          // use delay
-//import org.atnos.eff.syntax.eval._   // use runEval
 
-//import org.atnos.eff.option._        // use fromOption
-//import org.atnos.eff.syntax.option._ // use runOption
+import org.atnos.eff.either._
+import org.atnos.eff.syntax.either._
 
-//import org.atnos.eff.either._        // use optionEither
-//import org.atnos.eff.syntax.either._ // use runEither
-
-//import org.atnos.eff.syntax.eff._    // use run
-
+import org.atnos.eff.syntax.eff._
 
 import org.atnos.eff.all._
-import org.atnos.eff.syntax.all._
 
 
 object Main extends App {
-  type SO = Fx.fx1[Option]
-
-  val evalEff = delay(1 + 1).runEval.run
-  println(evalEff)
-
-
-  val mapO: Map[String, Int] =
-    Map("key1" -> 10, "key2" -> 20)
-
-
-  def addKeys(key1: String, key2: String): Eff[SO, Int] = for {
-    a <- fromOption(mapO.get(key1))
-    b <- fromOption(mapO.get(key2))
-  } yield a + b
-
-  val optionEff = (addKeys("key1", "key2").runOption.run, addKeys("key1", "missing").runOption.run)
-
-  println(optionEff)
 
   type SE = Fx.fx1[String Either ?] // use kind-projector
 
   val mapE: Map[String, Int] = Map("key1" -> 10, "key2" -> 20)
 
+  // intelliJ では解決してくれないけど動く
   def addKeys(key1: String, key2: String): Eff[SE, Int] = for {
     a <- optionEither(mapE.get(key1), s"'$key1' not found")
     b <- optionEither(mapE.get(key2), s"'$key2' not found")
   } yield a + b
 
-  (addKeys("key1", "key2").runEither.run, addKeys("key1", "missing").runEither.run)
+  val c = (addKeys("key1", "key2").runEither.run, addKeys("key1", "missing").runEither.run)
+  println(c)
 
 
 
+  type E = Fx.fx1[TooBig Either ?]
+
+  val i = 7
+
+  val value: Eff[E, Int] =
+    if (i > 5) left[E, TooBig, Int](TooBig(i))
+    else       right[E, TooBig, Int](i)
+
+  val action: Eff[E, Int] = catchLeft[E, TooBig, Int](value) { case TooBig(k) =>
+    if (k < 10) right[E, TooBig, Int](k)
+    else        left[E, TooBig, Int](TooBig(k))
+  }
+
+  action.runEither.run ==== Right(7)
 }
+
+case class TooBig(value: Int)
